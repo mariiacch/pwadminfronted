bb4efe61be3b00fdaf973153441305e18302f120
maria carrero <mariacarrerochacon267@gmail.com>
2022-01-21 15:07:21 -0400

    ...

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,7 +8,7 @@ import axios from "axios"
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("https://pwadmin.herokuapp.com/api/auth/login", userCredential);
+    const res = await axios.post("https://pwadmin.herokuapp.com/auth/login", userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     console.log(dispatch);
     
